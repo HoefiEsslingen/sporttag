6832642dc2fe5c596659b4035c74457561037433
Gerold Höferlin <gerold-hoeferlin@gmx.de>
2026-07-15 19:57:32 +0200

    Deploy: 2026-07-15 19:57:32

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1190744682" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2354812342" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
